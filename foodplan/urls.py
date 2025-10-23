20ecd654e9efd9182ea1d59959bb76f03a529dc0
mikrukovd <z123456689z@gmail.com>
2025-10-23 15:13:39 +0300

    Добавил urls для html из верстки

--- foodplan/urls.py
+++ foodplan/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',
-         views.index, name='index')
+    path('', views.index, name='index'),
+    path('auth/', views.auth, name='auth'),
+    path('registration/', views.registration, name='registration'),
+    path('lk/', views.lk, name='lk'),
+    path('card1/', views.card1, name='card1'),
+    path('card2/', views.card2, name='card2'),
+    path('card3/', views.card3, name='card3'),
+    path('order/', views.order, name='order'),
 ]
--- foodplan/views.py
+++ foodplan/views.py
@@ -5,3 +5,31 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def auth(request):
+    return render(request, 'auth.html')
+
+
+def registration(request):
+    return render(request, 'registration.html')
+
+
+def lk(request):
+    return render(request, 'lk.html')
+
+
+def card1(request):
+    return render(request, 'card1.html')
+
+
+def card2(request):
+    return render(request, 'card2.html')
+
+
+def card3(request):
+    return render(request, 'card3.html')
+
+
+def order(request):
+    return render(request, 'order.html')
